5c35e34f0306e81fd5c4e17c7e32ad4b56cd72ee
Rodrigo Pituba de Souza <rodrigopitubasouza@live.com>
2018-12-05 15:19:38 -0200

    Processo de pegar os dados do JSON

---
+++ app/src/main/java/com/example/rodri/filmesseries/FilmeProcessor.java
@@ -0,0 +1,42 @@
+package com.example.rodri.filmesseries;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmeProcessor {
+    public static List<Filmes> process(String input) {
+        List<Filmes> filmes = new ArrayList<>();
+        if(input != null && input.startsWith("{")){
+
+            try {
+                JSONObject json = new JSONObject(input);
+
+                JSONArray results = json.getJSONArray("results");
+                if(results.length() > 0){
+                    for (int i = 0; i < results.length(); i++) {
+
+                        JSONObject c = results.getJSONObject(i);
+                        String titulo = c.getString("title");
+                        String id = c.getString("id");
+                        String popularidade = c.getString("popularity");
+                        String imagem = c.getString("poster_path");
+                        String descricao = c.getString("overview");
+                        String date = c.getString("release_date");
+
+                        Filmes filme = new Filmes(titulo, id, popularidade, descricao, date, imagem);
+                        filmes.add(filme);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return filmes;
+    }
+}
+
+
